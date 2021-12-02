e322f11f7263b40b21b49d5461ae574b715eae8a
Osaou <osaourell@gmail.com>
2021-12-02 23:35:48 +0100

    build aoc cli tool to tmp folder before moving to /usr/local/bin

--- build-aoc.sh
+++ build-aoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build aoc cli tool
-nim compile --out:./aoc aoc.nim
+nim compile --out:./tmp/aoc aoc.nim
 
 # move it to /usr/local/bin
-sudo mv ./aoc /usr/local/bin/aoc
+sudo mv ./tmp/aoc /usr/local/bin/aoc
