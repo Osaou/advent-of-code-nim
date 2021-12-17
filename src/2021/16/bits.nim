b47016ed2b0f3472171cfd598923a8bf6c29cbbb
Osaou <osaourell@gmail.com>
2021-12-17 03:44:43 +0100

    clean debug code from day 16 solutions

--- src/2021/16/bits.nim
+++ src/2021/16/bits.nim
@@ -77,8 +77,6 @@ proc parseOperator(binary: var string): tuple[parsed:int, mode:OperatorMode, sub
 
 
 proc parseBitsPacket(packetHex: string): Packet =
-  echo "parsing bits packet: ", packetHex
-  echo "binary: ", packetHex.items.toSeq().map(hexToBin).join(",")
   var binary = packetHex.items.toSeq().map(hexToBin).join()
   parsePacket(binary).packet
 
@@ -96,7 +94,6 @@ func packetKindToOperatorKind(header: PacketHeader): OperatorKind =
     else: raise newException(ValueError, "error in input data")
 
 proc parsePacket(binary: var string): tuple[parsed:int, packet:Packet] =
-  echo "PACKET, starting at: ", binary
   let (headerLength, header) = parseHeader(binary)
 
   case header.typeId:
@@ -125,7 +122,6 @@ proc parsePacket(binary: var string): tuple[parsed:int, packet:Packet] =
       )
 
 proc parseHeader(binary: var string): tuple[parsed:int, header:PacketHeader] =
-  echo " HEADER, starting at: ", binary
   let
     version = binary[0..<3].binToInt()
     typeId = binary[3..<6].binToInt()
@@ -133,8 +129,6 @@ proc parseHeader(binary: var string): tuple[parsed:int, header:PacketHeader] =
   # advance past header
   binary = binary[6 ..< binary.len]
 
-  echo fmt"  version: ", version
-  echo fmt"  typeId: ", typeId.TypeId
   (
     parsed: 6,
     header: PacketHeader(
@@ -144,7 +138,6 @@ proc parseHeader(binary: var string): tuple[parsed:int, header:PacketHeader] =
   )
 
 proc parseLiteral(binary: var string): tuple[parsed:int, value:int64] =
-  echo " LITERAL, starting at: ", binary
   var
     literal = ""
     parsed = 0
@@ -158,27 +151,12 @@ proc parseLiteral(binary: var string): tuple[parsed:int, value:int64] =
     # advance past value
     binary = binary[5 ..< binary.len]
 
-  echo "  literal: ", literal
-  echo "  value: ", literal.binToInt64()
-
-  #let
-  #  literalLength = literal.len * 5 + 6 # include length of literal's header (version and type id)
-  #  rem = literalLength / 4
-  #  padding = rem.ceil.int * 4 - literalLength
-  #
-  #echo "  padding: ", padding
-  #
-  ## advance past padding
-  #if padding > 0:
-  #  binary = binary[padding ..< binary.len]
-
   (
     parsed: parsed,
     value: literal.binToInt64()
   )
 
 proc parseOperator(binary: var string): tuple[parsed:int, mode:OperatorMode, subPackets:seq[Packet]] =
-  echo " OPERATOR, starting at: ", binary
   var
     parsed = 0
     subPackets: seq[Packet]
@@ -188,58 +166,32 @@ proc parseOperator(binary: var string): tuple[parsed:int, mode:OperatorMode, sub
 
   case mode:
     of Zero:
-      echo "  operator mode 0"
       let subPacketDataLength = binary[1..15].binToInt()
-      echo "  subPacketDataLength: ", subPacketDataLength
 
       # advance past operator header
       binary = binary[16 ..< binary.len]
       parsed += 16
 
-      #let
-      #  beforeSubPackets = binary
-      #  rem = subPacketDataLength / 4
-      #  padding = rem.ceil.int * 4 - subPacketDataLength
-      #  totalSubLength = subPacketDataLength + padding
-      #echo "  padding ", padding
       var subPacketDataParsed = 0
 
       while subPacketDataParsed < subPacketDataLength:
         let (subPacketLength, subPacket) = parsePacket(binary)
         subPacketDataParsed += subPacketLength
-        echo "  parsed ", subPacketDataParsed, " out of ", subPacketDataLength, " total bits"
         subPackets &= subPacket
 
       parsed += subPacketDataParsed
 
-      # advance past sub-packets
-      #binary = beforeSubPackets[totalSubLength .. beforeSubPackets.len - 1]
-
-      #if padding > 0:
-      #  binary = binary[padding ..< binary.len]
-
     of One:
-      echo "  operator mode 1"
       let subPacketCount = binary[1..11].binToInt()
-      echo "  subPacketCount: ", subPacketCount
 
       # advance past operator header
       binary = binary[12 ..< binary.len]
       parsed += 12
 
-      #let
-      #  beforeSubPackets = binary
-      #  rem = subPacketDataLength / 4
-      #  padding = rem.ceil.int * 4 - subPacketDataLength
-      #  totalSubLength = subPacketDataLength + padding
-
       for sp in 1..subPacketCount:
         let (subPacketLength, subPacket) = parsePacket(binary)
         parsed += subPacketLength
         subPackets &= subPacket
-
-      # advance past sub-packets
-      #binary = beforeSubPackets[totalSubLength .. beforeSubPackets.len - 1]
 
   (
     parsed: parsed,
