39186e0c045374d9c9aee932de1b01626e18c543
Osaou <osaourell@gmail.com>
2024-01-09 02:28:53 +0100

    more progress on day 17

--- src/2023/17/1.nim
+++ src/2023/17/1.nim
@@ -1,4 +1,4 @@
-import std/[strformat, strutils, sequtils, sugar, tables, sets, options, math, algorithm, heapqueue]
+import std/[strformat, strutils, sequtils, sugar, tables, hashes, sets, options, math, algorithm, heapqueue]
 import fusion/matching
 import utils
 import matrix
@@ -16,10 +16,96 @@ type
     state: ProgressState
     cost: int
     priority: int
+    #steps: int
     prev: Node
+  PQNode = ref object
+    data: Node
+    priority: int
+    prev, next: PQNode
+  PQ = ref object
+    head, tail: PQNode
+    size: int
+  # [
+  MovementKind = enum
+    Straight
+    Turn
+    Loop
+  Movement = object
+    case kind: MovementKind
+    of Straight: len: int
+    of Turn: straight: int
+    of Loop: discard
+  #]#
 
-# [
-proc `hash`(x: ProgressState): int =
+proc newPrioQueue(): PQ =
+  PQ(head:nil, tail:nil, size:0)
+
+proc push(queue: var PQ, data: Node, priority: int) =
+  # first node?
+  if queue.head == nil:
+    let node = PQNode(data:data, priority:priority, prev:nil, next:nil)
+    queue.head = node
+    queue.tail = node
+    queue.size = 1
+    return
+
+  var
+    prev: PQNode = nil
+    next = queue.head
+  while next != nil:
+    if priority < next.priority:
+      break
+
+    prev = next
+    next = next.next
+
+  # new tail?
+  if next == nil:
+    let node = PQNode(data:data, priority:priority, prev:queue.tail, next:nil)
+    if queue.tail != nil:
+      queue.tail.next = node
+    queue.tail = node
+  # new head?
+  elif prev == nil:
+    let node = PQNode(data:data, priority:priority, prev:nil, next:queue.head)
+    queue.head.prev = node
+    queue.head = node
+  # in the middle somewhere?
+  else:
+    let node = PQNode(data:data, priority:priority, prev:prev, next:next)
+    prev.next = node
+    next.prev = node
+
+  queue.size += 1
+
+proc pop(queue: var PQ): Node =
+  if queue.head == nil:
+    return nil
+
+  let data = queue.head.data
+  queue.head = queue.head.next
+
+  if queue.head == nil:
+    queue.tail = nil
+
+  queue.size -= 1
+  return data
+
+proc len(queue: PQ): int =
+  queue.size
+
+#[
+proc hash(x: ProgressState): Hash =
+  result =
+    x.pos.x.hash !&
+    x.pos.y.hash !&
+    x.dir.x.hash !&
+    x.dir.y.hash !&
+    x.straight.hash
+  result = !$result
+#]#
+#[
+proc hash(x: ProgressState): Hash =
   x.pos.y +
     x.pos.x * 1_000 +
     x.dir.y * 1_000_000 +
@@ -28,36 +114,32 @@ proc `hash`(x: ProgressState): int =
 #]#
 
 proc `<`(a, b: Node): bool =
-  #a.cost < b.cost
-
-  a.priority < b.priority
-  #a.priority < b.priority and a.cost < b.cost
-  #(a.priority div 3) < (b.priority div 3) and a.cost < b.cost
+  a.cost < b.cost
+  #a.priority < b.priority
 
   #[
-  let prio = a.priority - b.priority
-  #let prio = (a.priority div 3) - (b.priority div 3)
-  #let prio = (a.priority div 2) - (b.priority div 2)
-  #let prio = (a.priority - b.priority) + 2
-  if prio < 0: true
-  elif prio > 0: false
-  else: a.cost < b.cost
-  #]#
-
-  #(a.cost + a.priority) < (b.cost + b.priority)
+  if a.cost != b.cost:
+    return a.cost < b.cost
+  if a.state.straight != b.state.straight:
+    return a.state.straight < b.state.straight
+  return a.priority < b.priority
+  ]#
 
 proc solve*(input: string): int
 #proc dijkstra(grid: Matrix[int]): int
 proc dijkstra(grid: Matrix[int]): seq[Position]
-proc countStraightSteps(currentNode: Node, step: Position): int {.inline.}
-proc depthIndex*(dx,dy: int, straight: int): int
-proc manhattanDistance(a,b: Position): int {.inline.}
+proc countStraightSteps(currentNode: Node, dir: Position): int {.inline.}
+proc mapPreviousFourSteps(currentNode: Node, dir: Position): Movement {.inline.}
+#proc manhattanDistance(a,b: Position): int {.inline.}
 
 proc renderPath(grid: Matrix[int], path: seq[Position])
 proc renderStepFrom(a,b: Position): char
 
-#tests:
-#  solve(readFile("test.txt")) == 102
+tests:
+  solve(readFile("test.txt")) == 102
+  solve(readFile("test2.txt")) == 171
+  solve(readFile("test3.txt")) == 7
+  solve(readFile("test4.txt")) == 29
 #  solve(readFile("input.txt")) == 0
 
 
@@ -68,57 +150,38 @@ proc solve(input: string): int =
     .mapIt(it.toSeq.map(charToInt))
     .matrix
 
-  echo "finding the shortest path..."
   let path = dijkstra(grid)
 
   # [
-  echo "shortest path:"
   renderPath(grid, path)
   #]#
 
   let x = path.foldl(a + grid[b.y, b.x], 0)
+  #let x = path[^1].cost
   #echo x
   x
 
 #proc dijkstra(grid: Matrix[int]): int =
 proc dijkstra(grid: Matrix[int]): seq[Position] =
   let
-    start = Node(state:(pos:(0,0).Position, dir:(0,0).Position, straight:0).ProgressState, cost:0, priority:0, prev:nil)
-    goal = (grid.lastRow, grid.lastCol).Position
+    start = (0,0).Position
+    #startNode = Node(state:(pos:start, dir:(0,0).Position, straight:0).ProgressState, cost:0, priority:0, steps:0, prev:nil)
+    startNode = Node(state:(pos:start, dir:(0,0).Position, straight:0).ProgressState, cost:0, priority:0, prev:nil)
+    #startNode = Node(state:ProgressState(pos:start, dir:(0,0).Position, straight:0), cost:0, priority:0, steps:0, prev:nil)
+    goal = (grid.lastCol, grid.lastRow).Position
+
+  #echo startNode.state
+  #echo $startNode.state
 
   var
-    #distances = matrix[seq[int]](grid.rows, grid.cols)
-    #previous = matrix(grid.rows, grid.cols, (0,0).Position)
-    #cache = newTable[ProgressState, Node]()
-    cache = newTable[ProgressState, int]()
-    #cache: seq[seq[seq[seq[Node]]]] = newSeqWith(grid.rows, newSeqWith(grid.cols, newSeqWith(4, newSeqWith[Node](4, nil))))
-    queue = [start].toHeapQueue
-    #paths = newSeq[Node]()
+    cache = newTable[string, int]()
+    queue = [startNode].toHeapQueue
+    #queue = newPrioQueue()
+    recordNode: Node = nil
+    iterations = 0
 
-  #[
-  const DEPTH = 4 * 4
-  for y in 0 ..< distances.rows:
-    for x in 0 ..< distances.cols:
-      for z in 0 ..< DEPTH:
-        if y == 0 and x == 0:
-          distances[y,x] = newSeqWith(DEPTH, 0)
-        else:
-          distances[y,x] = newSeqWith(DEPTH, int.high)
-  ]#
-
-  #[
-  for y in 0 ..< grid.rows:
-    for x in 0 ..< grid.cols:
-      if y != 0 and x != 0:
-        let
-          pos = (x,y).Position
-          priority = manhattanDistance(pos, goal)
-          node = (pos, grid[y,x], priority).Node
-        queue.push(node)
-  ]#
-
-  var recordNode: Node = nil
-  var iterations = 0
+  echo "goal: ", goal.x, ", ", goal.y
+  #queue.push(startNode, 0)
 
   while queue.len > 0:
     let
@@ -126,179 +189,192 @@ proc dijkstra(grid: Matrix[int]): seq[Position] =
       nodePos = node.state.pos
     #echo "checking (", x, ",", y, ")"
 
-    if nodePos == goal:
-      #[
-      paths.add(node)
-      if paths.len >= 10:
-        break
-      ]#
+    #[
+    var
+      path = newSeq[Position]()
+      n = node
+    while n != nil:
+      path.add(n.state.pos)
+      n = n.prev
+    echo "current path: ", path.reversed()
+    #]#
 
+    #[
+    var
+      path = newSeq[Position]()
+      n = node
+    while n != nil:
+      path.add(n.state.pos)
+      n = n.prev
+    write(stdout, "cost: ")
+    write(stdout, node.cost)
+    write(stdout, ", path: ")
+    for p in path.reversed():
+      write(stdout, grid[p.y, p.x])
+    #write(stdout, ", path: ")
+    for p in path.reversed():
+      write(stdout, " (")
+      write(stdout, p.x)
+      write(stdout, ",")
+      write(stdout, p.y)
+      write(stdout, ")")
+    echo ""
+    #]#
+
+    if nodePos == goal:
+    #if nodePos.x == goal.x and nodePos.y == goal.y:
+      echo "found it!"
       # [
       recordNode = node
       break
       #]#
-
-      #[
-      if recordNode == nil or node.cost < recordNode.cost:
-        recordNode = node
-
-      continue
-      ]#
-
-      #[
-      echo "found it?"
-      if distances[goal.y, goal.x].anyIt(it == 0):
-        continue
-      else:
-        break
-      ]#
-
-      #[
-      echo "found it!"
-      #return distances[node.pos.y, node.pos.x]
-      var path = newSeq[Position]()
-      var n = node
-      while n.pos != start.pos:
-        path.add(n.pos)
-        n = n.prev
-      return path.reversed()
-      ]#
 
     let
       (cx, cy) = nodePos
       (px, py) = if node.prev == nil: (0,0)
                  else: node.prev.state.pos
 
-    for nei, neighbor in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+    for nei, dir in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
       let
-        (dx, dy) = neighbor
+        (dx, dy) = dir
         nx = cx + dx
         ny = cy + dy
 
       if nx == px and ny == py:
+        #echo "  discarded movement ", dir, " because of BACKTRACKING"
         continue
 
       if nx < 0 or
          ny < 0 or
          nx > grid.lastCol or
          ny > grid.lastRow:
+        #echo "  discarded movement ", dir, " because of OUTSIDE PERIMETER"
         continue
 
-      let
-        pos = (nx,ny).Position
-        straight = countStraightSteps(node, pos)
+      # [
+      let straight = countStraightSteps(node, dir)
       if straight > 3:
+        #echo "  discarded movement ", dir, " because of EXCEEDED MAX STRAIGHT STEPS (", straight, ")"
         continue
+      #]#
+      #[
+      let
+        steps = mapPreviousFourSteps(node, dir)
+        straight = case steps.kind
+        of Straight:
+          continue
+        else:
+          steps.straight
+      #]#
 
       let
-        #depth = depthIndex(dx,dy, straight)
-        #cost = grid[ny, nx]
         totalCost = node.cost + grid[ny, nx]
-        state = (pos:pos, dir:neighbor.Position, straight:straight).ProgressState
-
-      #[
-      var recordCost = int.high
-      let cached = cache.getOrDefault(state, nil)
-      #let cached = cache[ny][nx][nei][straight]
-      if cached != nil:
-        recordCost = cached.cost
-      ]#
-      let recordCost = cache.getOrDefault(state, int.high)
+        pos = (nx, ny).Position
+        state = (pos:pos, dir:dir, straight:straight).ProgressState
+        #state = ProgressState(pos:pos, dir:dir, straight:straight)
+        recordCost = cache.getOrDefault($state, int.high)
       if totalCost > recordCost:
         continue
 
+      #if recordCost != int.high:
+      #  queue.del(next) !!!
+
       let
-        #priority = manhattanDistance(pos, goal) # HOW DO I GET THE PRIORITY RIGHT??? cost and distance should both matter, right?
+        #distance = manhattanDistance(pos, goal)
+        #distance = manhattanDistance(start, pos)
+        #steps = node.steps + 1
         priority = totalCost
+        #next = Node(state:state, cost:totalCost, priority:priority, steps:steps, prev:node)
         next = Node(state:state, cost:totalCost, priority:priority, prev:node)
+        #present = queue.find(next)
+
+      #if present >= 0:
+      #  continue
+
       queue.push(next)
-      #cache[ny][nx][nei][straight] = cost
-      cache[state] = totalCost
+      #queue.push(next, priority)
+      cache[$state] = totalCost
+      #echo "  pushed movement ", dir, " with priority ", priority
 
-      #[
-      for z in 0 ..< DEPTH:
-        if totalCost < distances[ny, nx][z]:
-          var depthCost = distances[ny, nx]
-          depthCost[depth] = totalCost
-          #previous[ny, nx] = node.pos
-
-          let
-            priority = manhattanDistance(pos, goal)
-            next = Node(pos:pos, cost:cost, priority:priority, prev:node)
-          queue.push(next)
-      ]#
-
-    #[
+    # [
     iterations += 1
     if iterations mod 100_000 == 0:
       echo queue.len
       iterations = 0
     #]#
 
+  echo "iterations: ", iterations
+
   if recordNode == nil:
     return @[]
 
-  #return distances[node.pos.y, node.pos.x]
-
-  #[
-  recordNode = paths[0]
-  for i in 1 ..< paths.len:
-    let n = paths[i]
-    if n.cost < recordNode.cost:
-      recordNode = n
-  ]#
-
-  var path = newSeq[Position]()
-  var n = recordNode
-  while n.state.pos != start.state.pos:
+  var
+    path = newSeq[Position]()
+    n = recordNode
+  while n.state.pos != start:
     path.add(n.state.pos)
     n = n.prev
   return path.reversed()
 
-proc countStraightSteps(currentNode: Node, step: Position): int =
-  var node = currentNode
+# [
+proc countStraightSteps(currentNode: Node, dir: Position): int =
+  var
+    node = currentNode
+    steps = 1
+
   for i in 1..3:
     if node.prev == nil:
       break
+    if node.state.dir != dir:
+      break
+
     node = node.prev
+    steps += 1
+
+  steps
+#]#
+
+# [
+proc mapPreviousFourSteps(currentNode: Node, dir: Position): Movement =
+  var
+    node = currentNode
+    distance = 1
+    nextDir = dir
+
+  for i in 1..3:
+    if node.prev == nil:
+      break
+
+    if node.state.dir == nextDir:
+      distance += 1
+    else:
+      nextDir = (0,0).Position
+
+    node = node.prev
+
+  if distance > 3:
+    return Movement(kind: Straight, len: distance)
 
   let
     (nx, ny) = node.state.pos
-    dx = abs(nx - step.x)
-    dy = abs(ny - step.y)
+    (cx, cy) = currentNode.state.pos
+    dx = abs(nx - (cx + dir.x))
+    dy = abs(ny - (cy + dir.y))
 
-  max(dx, dy)
-
-#[ tests:#
-  depthIndex(-1,0, 0) == 0
-  depthIndex(-1,0, 1) == 1
-  depthIndex(-1,0, 2) == 2
-  depthIndex(-1,0, 3) == 3
-  depthIndex( 1,0, 0) == 4
-  depthIndex( 1,0, 1) == 5
-  depthIndex( 1,0, 2) == 6
-  depthIndex( 1,0, 3) == 7
-  depthIndex(0,-1, 0) == 8
-  depthIndex(0,-1, 1) == 9
-  depthIndex(0,-1, 2) == 10
-  depthIndex(0,-1, 3) == 11
-  depthIndex(0, 1, 0) == 12
-  depthIndex(0, 1, 1) == 13
-  depthIndex(0, 1, 2) == 14
-  depthIndex(0, 1, 3) == 15
-]#
-proc depthIndex(dx,dy: int, straight: int): int =
-  if dy == 0:
-    return straight + 1 + (if dx < 0: dx
-                           else: 2 + dx)
+  if dx == 0 and dy == 0:
+    return Movement(kind: Loop)
   else:
-    return straight + 9 + (if dy < 0: dy
-                           else: 2 + dy)
+    return Movement(kind: Turn, straight: distance)
+#]#
 
+#[
 proc manhattanDistance(a,b: Position): int =
   abs(a.x - b.x) + abs(a.y - b.y)
+]#
 
 proc renderPath(grid: Matrix[int], path: seq[Position]) =
+  echo grid
+
   var blocks = matrix[char](grid.rows, grid.cols)
   for y in 0 ..< grid.rows:
     for x in 0 ..< grid.cols:
